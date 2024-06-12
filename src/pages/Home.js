6a3370ffad24782778718d15bb6f8e65e475ea41
Kaleb Fox <kalebfox7@yahoo.com>
2024-06-12 18:25:34 -0400

    done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,38 @@
 import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar";
+
 
 function Home() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+   
+
+
+    async function getData(){
+      const resp = await fetch("http://localhost:4000/movies")
+      const fetchData = await resp.json()
+      setData(fetchData)
+      
+      
+    }
+    getData()
+
+  },[])
+
+const moviecards = data.map(movie => <MovieCard title={movie.title} id={movie.id} key={movie.id}/>)
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+      <NavBar />
+       <h1>Home Page</h1>
       </header>
       <main>
-        {/* Info goes here! */}
+        {moviecards}
       </main>
     </>
-  );
-};
+  )
+}
 
 export default Home;
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,14 +1,38 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  console.log(useParams)
+  const prams = useParams()
+
+  const movidId = prams.id
+  const [data, setData] = useState({genres: []})
+  useEffect(() => {
+    async function getData(){
+      const resp = await fetch("http://localhost:4000/movies/"+ movidId)
+      const fetchData = await resp.json()
+      console.log(resp)
+      setData(fetchData)
+    }
+    getData()
+
+  },[movidId])
+
+ 
+  const name = data.genres.map(genre =>  <span key={genre}>{genre}</span>)
+
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
-      </main>
+    <h1>{data.title}</h1>
+    <p>{data.time}</p>
+    <span>{name}</span>
+    </main>
     </>
   );
 };
